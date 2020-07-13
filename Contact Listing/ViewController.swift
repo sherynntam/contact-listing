2b8fa3dbb30417ca9cc35aa2befa6393d79544b0
sherynn.tam <sutine93@hotmail.com>
2020-07-13 20:27:15 +0800

    initial commit.

---
+++ Contact Listing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contact Listing
+//
+//  Created by Sherynn Tam on 13/07/2020.
+//  Copyright © 2020 Sherynn Tam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
